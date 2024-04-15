aba5d189364686af73672dc65d1a9b12afaf9ae5
logeshwaranselvm <logeshwarans159@gmail.com>
2024-04-15 14:01:03 +0530

    no Changes

---
+++ Switch.java
@@ -0,0 +1,28 @@
+class Switch {
+    public static void main(String a[]){
+        int n = 7;
+        switch(n){
+            case 1: 
+            System.out.print("monday");
+            break;
+            case 2:
+            System.out.print("tuesday");
+            break;
+            case 3:
+            System.out.print("wednesday");
+            break;
+            case 4:
+            System.out.print("thursday");
+            break;
+            case 5:
+            System.out.print("friday");
+            break;
+            case 6:
+            System.out.print("saturday");
+            break;
+            case 7:
+            System.out.print("sunday");
+
+        }
+    }
+} 
---
+++ switchnew.java
@@ -0,0 +1,14 @@
+class switchnew{
+    public static void main(String a[]){
+        String day = "sunday";
+        switch(day){
+            case "saturday" -> System.out.println("9am");
+            case "sunday" -> System.out.println("10am");
+            default -> System.out.println("6am");
+
+
+        }
+
+    
+    }
+}
